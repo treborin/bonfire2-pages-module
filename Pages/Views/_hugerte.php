4a537bc917c655769f34ae051c6f060aeb7ff212
Donatas Glodenis <dg@lapas.info>
2025-03-30 13:19:08 +0300

    Add link/unlink buttons to hugerte toolbar

--- Pages/Views/_hugerte.php
+++ Pages/Views/_hugerte.php
@@ -3,7 +3,7 @@ hugerte.init({
     height: 300,
     menubar: false,
     entity_encoding : 'raw',
-    plugins: 'code',
+    plugins: 'code link',
     setup: function (editor) {
         editor.on('blur', function () {
             htmx.ajax('POST', '<?= $url ?>', { target: '#content_error' });
@@ -12,7 +12,7 @@ hugerte.init({
     statusbar: false,
     toolbar: 'undo redo | ' +
         'bold italic | alignleft aligncenter alignright alignjustify | ' +
-        'bullist numlist outdent indent | removeformat | code | help',
+        'bullist numlist outdent indent | link unlink | removeformat | code | help',
 });
 
 // on htmx submit, trigger hugerte save = transfer text from editor to textarea
